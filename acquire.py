120e4231f931043245ae7576a97decf0ae9364d5
Dan Churchill <dan.churchill.satx@gmail.com>
2022-10-12 14:35:26 -0500

    completed acquire exercises

--- acquire.py
+++ acquire.py
@@ -2,6 +2,7 @@ from requests import get
 from bs4 import BeautifulSoup
 import os
 import re
+import pandas as pd
 
 def get_blog_articles():
     url = 'https://codeup.com/blog/'
@@ -15,7 +16,26 @@ def get_blog_articles():
         blog_url = re.findall(regexp, str(link))[0]
         response2 = get(blog_url, headers=headers)
         soup2 = BeautifulSoup(response2.content, 'html.parser')
-        content = soup2.find('div', id='main-content').text
+        content = soup2.find('div', id='main-content').text.strip()
         articles.append({'title' : soup2.title.string, 'content' : content})
-    return articles
+    return pd.DataFrame(articles)
+
+def get_news_articles():
+    articles = []
+
+    home_url = 'https://inshorts.com/en/read/'
+    cat_list = ['technology', 'sports', 'technology', 'entertainment']
+    for cat in cat_list:
+        response = get(home_url+cat)
+        soup = BeautifulSoup(response.content, 'html.parser')    
+        titles = soup.select('div.news-card-title.news-right-box')
+        contents = soup.select('div.news-card-content.news-right-box')
+
+        regexp = r'(.+?)\n\n'
+        for i in range(len(titles)):
+            content = re.findall(regexp, contents[i].text.strip())
+            title = re.findall(regexp, titles[i].text.strip())
+            articles.append({'title' : title[0], 'content' : content[0], 'category' : cat})
+    return pd.DataFrame(articles)
+
 
